f65b1384ba5c216a15284d611ffdd2d76f6219ac
arxing <meiswind711108@gmail.com>
2020-02-04 19:34:03 +0800

    新增flutter版本

--- dart/lib/messages_dart.dart
+++ dart/lib/messages_dart.dart
@@ -3,3 +3,4 @@ library messages_dart;
 export 'src/messages_handler.dart';
 export 'src/messages_model.dart';
 export 'src/messages_mapper.dart';
+export 'src/messages_extension.dart';
---
+++ flutter/lib/messages_flutter.dart
@@ -0,0 +1,4 @@
+library messages_flutter;
+
+export 'package:messages_dart/messages_dart.dart';
+export 'src/messages_extension.dart';
